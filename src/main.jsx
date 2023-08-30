d438cdea0d62d67059a21ae0e93dbe224705c413
mufeeda1996 <96809025+mufeeda1996@users.noreply.github.com>
2023-08-30 14:24:50 +0400

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ToastContainer />
       <React.StrictMode>
-        <App />
+        <App/>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
